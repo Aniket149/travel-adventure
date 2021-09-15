835a34a46c47675ccb2e64e499371e8a3705153d
Aniket149 <aniketgalande100@gmail.com>
2021-09-15 09:26:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Navbar/>
         <Home/>
         <Switch>
-          <Route path = "/" exact component={Home}/>
           <Route path = "/services" component={Services}/>
           <Route path = "/products" component={Products}/>
           <Route path = "/sign-up" component={SignUp}/>
